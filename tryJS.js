1204476560fafa7113f9c4a1435d6d17a6531ea7
Bofingao <102545376+Bofingao@users.noreply.github.com>
2022-03-29 20:08:32 +0300

    Add files via upload

---
+++ tryJS.js
@@ -0,0 +1,22 @@
+
+
+
+
+function onImage() {
+     document.getElementById('myImage').src='https://www.w3schools.com/js/pic_bulbon.gif';
+  }
+
+
+  
+function offImage() {
+     document.getElementById('myImage').src='https://www.w3schools.com/js/pic_bulboff.gif';
+}
+
+
+function date() {
+ 
+    document.getElementById('demo').innerHTML = Date();  } 
+
+
+
+
